dc3a965950b745262be92fc61ddd8d09b5a173b0
Marcos Francesquini <francesquini.personal@gmail.com>
2023-07-01 14:00:43 +0200

    node express

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express = require("express")
+
+app = express()
+
+app.get("/home",(request,response) => {
+    response.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/views/works.html")
+})
+
+app.listen(3000,()=> {console.log("Listening... Server on")})
